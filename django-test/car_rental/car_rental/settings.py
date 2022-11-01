d0e9239ee05fe551b907ac66932081440430bc00
sunnivaheggem <116080868+sunnivaheggem@users.noreply.github.com>
2022-11-01 15:19:47 +0100

    serializer
    added serializer

--- django-test/car_rental/car_rental/settings.py
+++ django-test/car_rental/car_rental/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "car_rental",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
---
+++ django-test/car_rental/car_rental/views.py
@@ -0,0 +1,12 @@
+from .models import Car
+from rest_framework.response import Response 
+from .serializers import CarSerializer
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.decoratorsimport api_view
+
+@api_view(['GET']) 
+def get_cars(request):
+    cars = Car.objects.all()
+    serializer = CarSerializer(cars, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
