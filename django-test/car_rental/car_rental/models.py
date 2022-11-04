aa9eb4058be0339c1efd768de5a91ffee07acd80
sunnivaheggem <116080868+sunnivaheggem@users.noreply.github.com>
2022-11-04 09:44:28 +0100

    model

--- django-test/car_rental/car_rental/models.py
+++ django-test/car_rental/car_rental/models.py
@@ -3,20 +3,19 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length = 50) 
     carmodel = models.CharField(max_length = 50)
-    year = models.CharField(max_length = 5) 
+    year = models.CharField(max_length = 10) 
     location = models.CharField(max_length = 50)
     status = models.CharField(max_length = 50) 
-    
-    def __str__(self):
-        return self.make + ' '+ + self.carmodel+ ' '+ + self.year + ' '+ + self.location + ' '+ + self.status
+
+    def __str1__(self):
+        return self.make + ' '+ + self.carmodel + ' '+ + self.year + ' '+ + self.location + ' '+ + self.status
 
 class Customer(models.Model):
     name = models.CharField(max_length = 50) 
     age = models.CharField(max_length = 3)
     address = models.CharField(max_length = 50)
- 
 
-    def __str1__(self):
+    def __str2__(self):
         return self.name + ' '+ + self.age + ' '+ + self.address
 
 class Employee(models.Model):
@@ -24,5 +23,5 @@ class Employee(models.Model):
     branch = models.CharField(max_length = 50)
     address = models.CharField(max_length = 50) 
 
-    def __str2__(self):
+    def __str3__(self):
         return self.name + ' '+ + self.branch + ' '+ + self.address
